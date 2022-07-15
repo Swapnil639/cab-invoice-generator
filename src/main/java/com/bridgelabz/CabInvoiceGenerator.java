7c78c9576a4864de0eb180ddc3714b9c976fb31a
Swapnil639 <swapnilpatil631997@gmail.com>
2022-07-15 23:12:41 +0530

    Added :Should return minimum fare

--- src/main/java/com/bridgelabz/CabInvoiceGenerator.java
+++
@@ -1,7 +0,0 @@
-package com.bridgelabz;
-
-public class CabInvoiceGenerator {
-    public static void main(String[] args) {
-        System.out.println("Welcome to cab invoice generator program");
-    }
-}
