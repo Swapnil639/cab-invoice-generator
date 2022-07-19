cf36db0713ec23d625ae02d797b0efaea26600b1
Swapnil639 <swapnilpatil631997@gmail.com>
2022-07-19 23:33:14 +0530

    Refactor :Refactoring the Test case and return the NO of ride ,total fare,avgFare

---
+++ src/test/java/com/bridgelabz/InvoiceGeneratorTest.java
@@ -0,0 +1,40 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class InvoiceGeneratorTest {
+    InvoiceGenerator generator;
+
+    @Before
+    public void setUp() {
+        generator = new InvoiceGenerator();
+    }
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        double distance = 2.0;
+        int time = 5;
+        double fare = generator.calculateFare(distance, time);
+        Assert.assertEquals(25, fare, 0.0);
+    }
+
+    @Test
+    public void givenLessDistanceOrTime_ShouldReturnMinFare() {
+        double distance = 0.1;
+        int time = 1;
+        double fare = generator.calculateFare(distance, time);
+        Assert.assertEquals(5, fare, 0.0);
+
+    }
+
+    @Test
+    public void givenMultipleRides_ShouldReturnInvoiceSummary() {
+        Ride[] rides = {new Ride(10.0, 15),
+                new Ride(0.1, 1)
+        };
+        InvoiceSummary invoiceSummary = generator.calculateFare(rides);
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 120);
+        Assert.assertEquals(expectedInvoiceSummary, invoiceSummary);
+    }
+}
