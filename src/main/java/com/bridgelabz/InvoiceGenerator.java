f94912a9f7063362b9791baa2a78be4bdee01bb7
Swapnil639 <swapnilpatil631997@gmail.com>
2022-07-20 06:31:11 +0530

    Added :The cab agency supports normal and premium rides

--- src/main/java/com/bridgelabz/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -3,21 +3,27 @@ package com.bridgelabz;
 public class InvoiceGenerator {
     private static final int COST_PER_TIME = 1;
     private static final double MINIMUM_COST_PER_KILOMETER = 10.0;
-    private static final double MINIMUM_FARE = 5.0;
+    private static final double MINIMUM_FARE = 5;
+    private static final int PREMIUM_COST_PER_TIME = 2;
+    private static final double PREMIUM_MINIMUM_COST_PER_KILOMETER = 20.0;
+    private static final double PREMIUM_MINIMUM_FARE = 20;
 
     public double calculateFare(double distance, int time) {
         double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
-        if (totalFare < MINIMUM_FARE)
-            return MINIMUM_FARE;
-        else
-            return totalFare;
+        return Math.max(totalFare, MINIMUM_FARE);
     }
-    public double calculateFare(Ride[] rides) {
+
+    public InvoiceSummary calculateFare(Ride[] rides) {
         double totalFare = 0;
         for (Ride ride : rides) {
             totalFare += this.calculateFare(ride.distance, ride.time);
         }
-        return totalFare;
+        return new InvoiceSummary(rides.length, totalFare);
+    }
+
+    public double calculatePremiumFare(double distance, int time) {
+        double totalFare = distance * PREMIUM_MINIMUM_COST_PER_KILOMETER + time * PREMIUM_COST_PER_TIME;
+        return Math.max(totalFare, PREMIUM_MINIMUM_FARE);
 
     }
 }
--- src/test/java/com/bridgelabz/InvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/InvoiceGeneratorTest.java
@@ -1,36 +1,51 @@
 package com.bridgelabz;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class InvoiceGeneratorTest {
+    InvoiceGenerator generator = new InvoiceGenerator();
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 2.0;
         int time = 5;
-        double fare = invoiceGenerator.calculateFare(distance, time);
+        double fare = generator.calculateFare(distance, time);
         Assert.assertEquals(25, fare, 0.0);
     }
 
     @Test
     public void givenLessDistanceOrTime_ShouldReturnMinFare() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 0.1;
         int time = 1;
-        double fare = invoiceGenerator.calculateFare(distance, time);
+        double fare = generator.calculateFare(distance, time);
         Assert.assertEquals(5, fare, 0.0);
+
     }
 
     @Test
-    public void givenMultipleRides_ShouldReturnTotalFare() {
-        InvoiceGenerator cabInvoiceGenerator = new InvoiceGenerator();
-        Ride[] rides = {new Ride(2.0, 5),
+    public void givenMultipleRides_ShouldReturnInvoiceSummary() {
+        Ride[] rides = {new Ride(10.0, 15),
                 new Ride(0.1, 1)
-
         };
-        double fare = cabInvoiceGenerator.calculateFare(rides);
-        Assert.assertEquals(30, fare, 0.0);
+        InvoiceSummary invoiceSummary = generator.calculateFare(rides);
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 120);
+        Assert.assertEquals(expectedInvoiceSummary, invoiceSummary);
+    }
+
+    @Test
+    public void givenTwoDifferentRidesPremiumAndNormalRide_FindTotalFareSummary() {
+        double distance = 10;
+        int time = 15;
+        Assert.assertEquals(115, generator.calculateFare(distance, time), 0.0);
+        Assert.assertEquals(230, generator.calculatePremiumFare(distance, time), 0.0);
+    }
+
+    @Test
+    public void premiumMinimumFare() {
+        double distance = 0.1;
+        int time = 1;
+        Assert.assertEquals(20, generator.calculatePremiumFare(distance, time), 0.0);
 
     }
 }
